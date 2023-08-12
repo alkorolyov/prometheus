e20bf1c58cffa5502a9c96f6f9c92d9a9be1783d
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2023-08-12 18:47:53 +0200

    verbose output

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ###### node_exporter service ########
 
-# download latest node_exporter
+echo "download and extract latest node_exporter"
 cd /tmp
-wget $(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "browser_download_url.*linux-amd64" | cut -d '"' -f 4)
-
+wget -q $(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "browser_download_url.*linux-amd64" | cut -d '"' -f 4)
 tar vxf node_exporter*.tar.gz
 cd node_exporter*/
 
-# create user/group
+echo "create user/group"
 sudo useradd -rs /bin/false node_exporter
 sudo cp node_exporter /usr/local/bin
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
+echo "create service file"
 SERVICE="\n
 [Unit]\n
 Description=Node Exporter\n
@@ -30,10 +30,12 @@ WantedBy=multi-user.target\n
 
 sudo bash -c "echo -e '$SERVICE' > /etc/systemd/system/node_exporter.service"
 
+echo "start service"
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
 sudo systemctl status node_exporter.service
 sudo systemctl enable node_exporter
 
+echo "delete tmp files"
 rm -rf /tmp/node_exporter*/
 rm -rf /tmp/node_exporter*
