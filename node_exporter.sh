a7140e11ae31857b3a82a0b71b202ef7cd27a00a
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2023-08-12 18:25:35 +0200

    Create node_exporter.sh

---
+++ node_exporter.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+###### node_exporter service ########
+
+# download latest node_exporter
+cd /tmp
+wget $(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "browser_download_url.*linux-amd64" | cut -d '"' -f 4)
+
+tar vxf node_exporter*.tar.gz
+cd node_exporter*/
+
+# create user/group
+sudo useradd -rs /bin/false node_exporter
+sudo cp node_exporter /usr/local/bin
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+SERVICE="\n
+[Unit]\n
+Description=Node Exporter\n
+After=network-online.target\n
+\n
+[Service]\n
+User=node_exporter\n
+Group=node_exporter\n
+Type=simple\n
+ExecStart=/usr/local/bin/node_exporter\n
+\n
+[Install]\n
+WantedBy=multi-user.target\n
+"
+
+sudo bash -c "echo -e '$SERVICE' > /etc/systemd/system/node_exporter.service"
+
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl status node_exporter.service
+sudo systemctl enable node_exporter
+
+rm -rf /tmp/node_exporter*/
