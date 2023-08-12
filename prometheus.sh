b8054060b1cba6f542098e1a424d22d93ff91401
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2023-08-12 18:59:27 +0200

    force rewrite

--- prometheus.sh
+++ prometheus.sh
@@ -14,7 +14,6 @@ wget -q $(curl -s https://api.github.com/repos/prometheus/prometheus/releases/la
 tar vxf prometheus*.tar.gz
 cd prometheus*/
 
-
 echo "create config"
 CONFIG_CONTENT="
 global:\n
@@ -35,17 +34,17 @@ remote_write:\n
 echo -e $CONFIG_CONTENT > prometheus.yml
 
 echo "move files and change ownerships"
-mv prometheus /usr/local/bin
-mv promtool /usr/local/bin
+cp -f prometheus /usr/local/bin
+cp -f promtool /usr/local/bin
 chown prometheus:prometheus /usr/local/bin/prometheus
 chown prometheus:prometheus /usr/local/bin/promtool
 
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
 
-mv consoles /etc/prometheus
-mv console_libraries /etc/prometheus
-mv prometheus.yml /etc/prometheus
+cp -rf consoles /etc/prometheus
+cp -rf console_libraries /etc/prometheus
+cp -f prometheus.yml /etc/prometheus
 chown prometheus:prometheus /etc/prometheus
 chown -R prometheus:prometheus /etc/prometheus/consoles
 chown -R prometheus:prometheus /etc/prometheus/console_libraries
@@ -84,4 +83,3 @@ rm -rf /tmp/prometheus*/
 rm -rf /tmp/prometheus*
 
 echo "finished!"
-
